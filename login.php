5b26832adbda0330e4190499e0d2bd2ef6c4b291
Robson Andrei dos Santos <robsonsantos@MacBook-Pro-de-Robson.local>
2022-01-13 17:45:21 -0300

    Conexao ok login2

--- login.php
+++ login.php
@@ -5,3 +5,8 @@ if(empty($_POST['usuario'])|| empty($_POST['senha'])){
     header('Location: index.php');
     exit();
 }
+
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+$query = "select id_usuario, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
