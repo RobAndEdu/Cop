179a9e45579fbf9fa6e3a32fd61c2d077919d3d1
Robson Andrei dos Santos <robsonsantos@MacBook-Pro-de-Robson.local>
2022-01-13 16:48:54 -0300

    Atualização arquivos PHP

--- css/conexao.php
+++ css/conexao.php
@@ -1,2 +1,7 @@
-<?php>
-</php>
+<?php 
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', 'sm1ct2021');
+define('COP', 'login');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, COP) or die ('Não foi possivel conectar');
