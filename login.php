427bc14b894a224a26e9cc71f7cd91413f86591f
Robson Andrei dos Santos <robsonsantos@MacBook-Pro-de-Robson.local>
2022-01-14 10:32:14 -0300

    Atualizando a conexao5

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(empty($_POST['usuario'])|| empty($_POST['senha'])){
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$query = "select id_usuario, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$query = "select id_usuario, usuario from Usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
 
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
